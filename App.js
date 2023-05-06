75e9c4cb62e4c1a313984b75fdccc8414001265c
Krzysiek <kurzy115@gmail.com>
2023-05-06 11:23:37 +0200

    add components

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.taskswrapper}>
         <Text style={styles.sectionTitle}>Tytuł taska</Text>
-        <View style={styles.items}></View>
+        <View style={styles.items}>
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+        </View>
       </View>
     </View>
   );
